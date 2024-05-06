a224e12d2d8e05478f77220cfa93d2d15ad5337d
Pedro Nieto <pedro.nieto@essentia.com.br>
2024-05-06 16:47:42 -0300

    feat: add Feature process

--- commit.sh
+++ commit.sh
@@ -36,7 +36,7 @@ if [ "$COMMIT_TYPE" = "Feature" ]; then
             clear
             gum log --level info "Feature criada com sucesso!"
             gum style \
-                --foreground "default" --align left --margin "0" --padding "0" "Branch atual: " --foreground "#45e4d7" $SCOPE
+                --foreground "default" --align left --margin "0" --padding "0" "Branch atual: " $SCOPE
 
             gum confirm "Deseja adicionar arquivos?" && {
             gum spin --spinner dot --title "Adicionando arquivos..." -- sh -c `clear && git add . && git status`
